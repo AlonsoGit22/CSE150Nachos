6de4e23e01361663d1350e1937bb1f81fd6e83d8
rbuo <56644469+rbuo@users.noreply.github.com>
2019-10-29 15:20:03 -0700

    Update Communicator.java

--- Communicator.java
+++ Communicator.java
@@ -14,11 +14,12 @@ public class Communicator {
     private Lock lock; //the lock for the communicators
     private Condition speakerSend; //used to send message
     private Condition listenerReceive; //used to receive message
-    private int speaker; //used to tell if there is a speaker
+
+    //private int speaker; //used to tell if there is a speaker
     private int listener; //used to tell if there is a listener
     //private boolean sent; //used to confirm speaker message has been sent
     private boolean speakerReady;
-    private boolean listenerReady;
+    //private boolean listenerReady;
     private boolean received; //used to confirm listener has been received
 
     private int message; //the message
@@ -36,7 +37,7 @@ public class Communicator {
         listener = 0;
 
         speakerReady = false;
-        listenerReady = false;
+        //listenerReady = false;
         received = false; 
     }
 
@@ -52,24 +53,32 @@ public class Communicator {
      */
     public void speak(int word) {
         lock.acquire();
-        while(speakerReady){
-            speakerSend.sleep(); //puts next speaker in queue
+        while(speakerReady){ //first speaker skips this
+            speakerSend.sleep(); //puts next speaker in send queue if there is a speaker ready already
         }
-        speakerReady = true; //
-        message = word;
-
-
-        while(listener == 0 || !received){ //if there are no listeners waiting, then wake the receive queue to send message
-            listenerReceive.wake();
-            speakerSend.sleep();
+        speakerReady = true; //marks the speaker as true, makes the while loop active
+        message = word; //transfer the word
+        
+        //skip while loop if listener is 0, or message is not received
+        while(listener == 1 && !received){ //if there are listeners waiting, then wake the receive queue to send message
+            listenerReceive.wake(); //wake up listeners in receive queue
+            speakerSend.sleep(); //put speaker in send queue
+            received = true;
         }
-        listener--;
-        received = false;//sets receive as false after sending the other messages
-        listenReceive.wake(); //wakes next listener
+        /*
+        while(listener == 0 && !received){ //if there are no listeners waiting, then wake the receive queue to send message
+            listenerReceive.wake(); //wake up listeners just incase
+            speakerSend.sleep(); //goes to sleep after attempting to wake listeners
+        }
+        */
+
+        //listener--;
+        
+        listenerReceive.wake(); //wakes next listener
         //speaker--;
         speakerSend.wake(); //wakes next speaker
-        //listenerReceive.wake(); //this might cause the communicator to return the word twice
-        
+        received = false;//sets receive as false after sending the other messages
+        //listenerReceive.wake(); //this might cause the communicator to return the word twice 
         lock.release(); 
     }
 
@@ -83,22 +92,18 @@ public class Communicator {
     public int listen() {
 
         lock.acquire();
-        listener++;
-        //listenerReady.wake();
-        while(listener > 0 && speakerReady){
-            listenerReceive.wake(); //puts next listener in queue
+        listener++; //listener at 1
+
+        while(!speakerReady){ //while speakerReady = false
+            listenerReceive.sleep();//puts listener in receive queue
         }
         
-        while(!speakerReady){ //puts listener to sleep if there are no speakers ready or the message has been received
-            listenerReceive.sleep();
-        }
-        listen--;
-        listenerReceive.sleep();
-        speakerSend.wake();
+        listenerReceive.sleep();//sleeps current receive
+        //speakerSend.wake();
 
-        received = true;
+        listener--;//listener back to 0
+
         //listenerWaiting = false;
-
         //listenerReceive.wake(); 
         
         lock.release();
