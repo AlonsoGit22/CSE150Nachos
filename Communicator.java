3a9c0ed9236a496b31cd76a99c2aab4bf6b0c4d6
rbuo <56644469+rbuo@users.noreply.github.com>
2019-10-28 20:27:18 -0700

    Update Communicator.java

--- Communicator.java
+++ Communicator.java
@@ -11,33 +11,29 @@ import nachos.machine.*;
  */
 public class Communicator {
 
-	private Lock lock; //the lock for the communicators
-    private Condition speakerQueue; //queue for speakers
-    private Condition listenerQueue; //queue for listeners
-
-    private boolean speakerWaiting; //used to tell if there is a speaker waiting
-    private boolean listenerWaiting; //used to tell if there is a listener waiting
-    
+    private Lock lock; //the lock for the communicators
     private Condition speakerSend; //used to send message
     private Condition listenerReceive; //used to receive message
-
-    private boolean sent; //used to confirm speaker message has been sent
+    private int speaker; //used to tell if there is a speaker
+    private int listener; //used to tell if there is a listener
+    //private boolean sent; //used to confirm speaker message has been sent
     private boolean received; //used to confirm listener has been received
 
     private int message; //the message
+
+
+
     /**
      * Allocate a new communicator.
      */
     public Communicator() {
-    lock = new Lock();
-    speakerQueue = new Condition(lock);
-    listenerQueue = new Condition(lock);
-    speakerSend = new Condition(lock);
-    listenerReceive = new Condition(lock);
-    speakerWaiting = false;
-    listenerWaiting = false;
-    received = false;
-    sent = false;
+        lock = new Lock();  
+        speakerSend = new Condition(lock);
+        listenerReceive = new Condition(lock);
+        speaker = 0;
+        listener = 0;
+    
+        received = false; 
     }
 
     /**
@@ -48,54 +44,55 @@ public class Communicator {
      * Does not return until this thread is paired up with a listening thread.
      * Exactly one listener should receive <i>word</i>.
      *
-     * @param	word	the integer to transfer.
+     * @param   word    the integer to transfer.
      */
     public void speak(int word) {
-    	lock.acquire();
-    	while(speakerWaiting){
-    		speakerQueue.sleep(); //puts next speaker in queue
-    	}
+        lock.acquire();
+        while(speaker == 0){
+            speakerSend.sleep(); //puts next speaker in queue
+        }
+        speaker++; //
+        message = word;
 
-    	speakerWaiting = true;
-    	message  = word;
+        while(listener != 0 || !received){ //if there are no listeners waiting, then wake the receive queue to send message
+            listenerReceive.wake();
+            speakerSend.sleep();
+        }
 
-    	while(!listenerWaiting || !received || !sent){ //if there are no listeners waiting, then wake the receive queue to send message
-    		listenerReceive.wake();
-    		speakerSend.sleep();
-    	}
-    	
-    	sent = true;
-    	speakerWaiting = false;
-    	speakerQueue.wake(); //wakes next speaker
-    	lock.release(); 
+        received = false;
+        speakerSend.wake(); //wakes next speaker
+        //listenerReceive.wake(); //this might cause the communicator to return the word twice
+        
+        lock.release(); 
     }
+
 
     /**
      * Wait for a thread to speak through this communicator, and then return
      * the <i>word</i> that thread passed to <tt>speak()</tt>.
      *
-     * @return	the integer transferred.
+     * @return  the integer transferred.
      */    
     public int listen() {
 
-   		lock.acquire();
-   		//listenerReady.wake();
-   		while(listenerWaiting){
-   			listenerQueue.sleep(); //puts next listener in queue
-   		}
+        lock.acquire();
+        //listenerReady.wake();
+        while(listener == 0){
+            listenerReceive.sleep(); //puts next listener in queue
+        }
+        listener++;
 
-   		listenerWaiting = true;
+        while(speaker == 0){ //puts listener to sleep if there are no speakers ready or the message has been received
+            listenerReceive.sleep();
+        }
+        //speakerSend.wake();
 
-   		while(!speakerWaiting){ //puts listener to sleep if there are no speakers ready or the message has been received
-   			listenerReceive.sleep();
-   		}
-   		speakerSend.wake();
+        received = true;
+        //listenerWaiting = false;
 
-   		received = true;
-   		listenerWaiting = false;
-
-   		listenerQueue.wake(); 
-   		lock.release();
-   		return message;
+        //listenerQueue.wake(); 
+        listener--;
+        lock.release();
+        return message;
     }
 }
